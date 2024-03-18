cc7e4a7cf8bb31618e52af59029ad82aa8d98433
Le Anh Minh <18521098@gm.uit.edu.vn>
2024-03-18 18:36:16 +0700

    draft:book store

---
+++ BookStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookStore
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hehe");
+        }
+    }
+}
